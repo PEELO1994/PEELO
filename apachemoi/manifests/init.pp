518152cc038c93c4ad9b75a5cda479e2ccddf50d
Ilari Huura <peelo1994@gmail.com>
2017-04-17 08:53:05 +0000

    Add apachemoi

---
+++ apachemoi/manifests/init.pp
@@ -0,0 +1,16 @@
+class apachemoi {
+
+    exec { 'apt-update':
+            command => '/usr/bin/apt-get update'
+    }
+
+    package { apache2:
+            require => Exec['apt-update'],
+            ensure => 'installed',
+            allowcdrom => 'true',
+    }
+
+    file {'/var/www/html/index.html':
+            content => "moi ilari hyvät setit siellä",
+    }
+}
