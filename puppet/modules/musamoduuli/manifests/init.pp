35054479028263ed057f3c8e3c21e3e94c5f593a
Ilari Huura <peelo1994@gmail.com>
2017-05-03 12:31:31 +0000

    added audacity

--- puppet/modules/musamoduuli/manifests/init.pp
+++ puppet/modules/musamoduuli/manifests/init.pp
@@ -1,14 +1,34 @@
 class musamoduuli {
 
-    exec { 'apt-update':
-            command => '/usr/bin/apt-get update'
-    }
+	exec { 'apt-update':
+		command => '/usr/bin/apt-get update'
+	}
 
-    package { lmms:
-            require => Exec['apt-update'],
-            ensure => 'installed',
-            allowcdrom => 'true',
-    }
+	package { lmms:
+		require => Exec['apt-update'],
+		ensure => 'installed',
+		allowcdrom => 'true',
+	}
 
+	package { guitarix:
+		require => Exec['apt-update'],
+		ensure => 'installed',
+		allowcdrom => 'true',
+
+	}
+
+	package { hydrogen:
+		require => Exec['apt-update'],
+                ensure => 'installed',
+                allowcdrom => 'true',
+
+	}
+	
+	package { audacity:
+                require => Exec['apt-update'],
+                ensure => 'installed',
+                allowcdrom => 'true',
+
+	}
    
 }
